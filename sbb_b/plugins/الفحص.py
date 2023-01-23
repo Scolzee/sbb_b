87ab26c483facbb22dda3247fad5f0ef5fa206f8
Scolzee <109732376+Scolzee@users.noreply.github.com>
2023-01-23 11:04:50 +0200

    Update الفحص.py

--- sbb_b/plugins/الفحص.py
+++ sbb_b/plugins/الفحص.py
@@ -123,8 +123,10 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
                 [
                     Button.url("قنـاة السـورس ⚙️", "https://t.me/Ch_frozen"),
                     Button.url("المطـور 👨🏼‍💻", "https://t.me/Scolze_WA"),
-                    Button.url("مبرمج السورس 👨🏼‍💻", "https://t.me/@M0_DW"),
                 ]
+                [ 
+                    Button.url("مبرمج السورس 👨🏼‍💻", "https://t.me/M0_DW"),
+                ] 
             ]
             if ROZ_PIC and ROZ_PIC.endswith((".jpg", ".png", "gif", "mp4")):
                 result = builder.photo(
